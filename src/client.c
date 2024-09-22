74585f7d80babdecb5c3f9e35faa95ba273b34ff
otema666 <otema000@gmail.com>
2024-09-22 21:08:56 +0200

    Mejoras en manejo de archivos y notificaciones
    Se han añadido nuevas entradas al archivo `.gitignore` para excluir archivos específicos de usuario y archivos generados automáticamente por MonoDevelop/Xamarin Studio, incluyendo `FodyWeavers.xsd` en `MigrationBackup/`.
    
    En `client.c`, se han añadido las bibliotecas `winsock2.h` y `ws2tcpip.h`, y se han definido la constante `MAX_MESSAGE_LENGTH` y la variable `CLASS_NAME`. Se han añadido variables globales `ip_address`, `port` y `download_directory`.
    
    La función `ev_handler_client` ahora incluye la llamada a `descargar_archivo` cuando se detecta un cambio en el archivo. La función `notificar` ha sido reemplazada con una nueva implementación que crea una ventana de notificación.
    
    Se ha añadido la función `descargar_archivo` para descargar archivos desde el servidor utilizando sockets. En `start_client`, se han eliminado las variables locales `ip` y `port`, reemplazándolas con las variables globales `ip_address` y `port`, y se ha añadido una llamada a `select_directory`.
    
    En `client.h`, se ha cambiado el tipo de `filename` en la estructura `FileInfo` de `char` a `wchar_t`, y se han añadido las declaraciones de las funciones `notificar` y `descargar_archivo`.

--- src/client.c
+++ src/client.c
@@ -7,11 +7,22 @@
 #include <shellapi.h>
 #include "mongoose.h"
 #include "client.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAX_MESSAGE_LENGTH 256
+const wchar_t CLASS_NAME[] = L"NotificationWindow";
+
+char ip_address[16];
+int port;
 
 FileInfo files[MAX_FILES];
 int file_count = 0;
 FileInfo previous_files[MAX_FILES];
 int previous_file_count = 0;
+wchar_t download_directory[MAX_PATH];
 
 void ev_handler_client(struct mg_connection* c, int ev, void* ev_data) {
     if (ev == MG_EV_HTTP_MSG) {
@@ -26,6 +37,7 @@ void ev_handler_client(struct mg_connection* c, int ev, void* ev_data) {
                     found = 1;
                     if (files[i].timestamp != previous_files[j].timestamp) {
                         notificar(files[i].filename);
+                        descargar_archivo(files[i].filename);
                     }
                     break;
                 }
@@ -42,41 +54,156 @@ void ev_handler_client(struct mg_connection* c, int ev, void* ev_data) {
     }
 }
 
-void notificar(const char* filename) {
-    NOTIFYICONDATAW nid;
-    memset(&nid, 0, sizeof(NOTIFYICONDATAW));
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
 
-    nid.cbSize = sizeof(NOTIFYICONDATAW);
-    nid.hWnd = NULL;
-    nid.uFlags = NIF_INFO | NIF_ICON;
-    nid.uTimeout = 10000;
-    nid.dwInfoFlags = NIIF_INFO;
+        SetTextColor(hdc, RGB(208, 229, 20));
+        SetBkMode(hdc, TRANSPARENT);
+        HFONT hFont = CreateFont(17, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
+            DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+            CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY,
+            VARIABLE_PITCH, TEXT("Arial"));
+        SelectObject(hdc, hFont);
 
-    HICON hIcon = LoadImageW(NULL, L"res\\r_icon.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE);
-    if (hIcon == NULL) {
-        DWORD error = GetLastError();
-        printf("Error al cargar el icono: %lu\n", error);
-        hIcon = LoadIconW(NULL, IDI_QUESTION);
+        const TCHAR* text = (const TCHAR*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        DrawText(hdc, text, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+        DeleteObject(hFont);
+        EndPaint(hwnd, &ps);
+    } break;
+
+    case WM_DESTROY: {
+        PostQuitMessage(0);
+    } break;
+
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-    nid.hIcon = hIcon;
+    return 0;
+}
 
-    wchar_t info[256];
-    wchar_t infoTitle[64];
+void notificar(const char* message) {
+    wchar_t w_message[MAX_MESSAGE_LENGTH];
+    MultiByteToWideChar(CP_UTF8, 0, message, -1, w_message, MAX_MESSAGE_LENGTH);
 
-    swprintf(info, sizeof(info) / sizeof(wchar_t), L"Archivo %S guardado con éxito", filename);
+    WNDCLASS wc = { };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClass(&wc);
 
-    MultiByteToWideChar(CP_UTF8, 0, "RStudio", -1, infoTitle, 64);
-    
-    wcsncpy_s(nid.szInfo, sizeof(nid.szInfo) / sizeof(wchar_t), info, _TRUNCATE);
-    wcsncpy_s(nid.szInfoTitle, sizeof(nid.szInfoTitle) / sizeof(wchar_t), infoTitle, _TRUNCATE);
+    HWND hwnd = CreateWindowEx(
+        WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_LAYERED | WS_EX_NOACTIVATE,
+        CLASS_NAME,
+        L"Notificación",
+        WS_POPUP,
+        GetSystemMetrics(SM_CXSCREEN) - 200,
+        GetSystemMetrics(SM_CYSCREEN) - 80,
+        180, 50,
+        NULL,
+        NULL,
+        GetModuleHandle(NULL),
+        NULL
+    );
 
-    Shell_NotifyIconW(NIM_ADD, &nid);
-
-    Shell_NotifyIconW(NIM_DELETE, &nid);
-
-    if (hIcon) {
-        DestroyIcon(hIcon);
+    if (hwnd == NULL) {
+        return;
     }
+
+    SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)w_message);
+
+    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    Sleep(1300);
+
+    DestroyWindow(hwnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+void descargar_archivo(const char* filename) {
+    SOCKET sock;
+    struct sockaddr_in server;
+    char source_url[256];
+    char buffer[4096];
+    int bytes_received;
+    FILE* fp;
+
+    snprintf(source_url, sizeof(source_url), "http://%s:%d/%s", ip_address, port, filename);
+
+    char* ip = strtok(source_url + 7, ":");
+    char* port_str = strtok(NULL, "/");
+    int port_number = atoi(port_str);
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        printf("Error al crear el socket: %d\n", WSAGetLastError());
+        return;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port_number);
+    inet_pton(AF_INET, ip, &server.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        printf("Error al conectar al servidor: %d\n", WSAGetLastError());
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+
+    char request[512];
+    snprintf(request, sizeof(request), "GET /%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", filename, ip);
+    send(sock, request, strlen(request), 0);
+
+    char destination_path[MAX_PATH];
+    snprintf(destination_path, sizeof(destination_path), "%s\\%s", download_directory, filename);
+    fp = fopen(destination_path, "wb");
+    if (fp == NULL) {
+        printf("Error al crear el archivo: %s\n", destination_path);
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+
+    int header_received = 0;
+    int header_length = 0;
+    while ((bytes_received = recv(sock, buffer, sizeof(buffer), 0)) > 0) {
+        if (!header_received) {
+            for (int i = 0; i < bytes_received; i++) {
+                if (buffer[i] == '\r' && buffer[i + 1] == '\n' && buffer[i + 2] == '\r' && buffer[i + 3] == '\n') {
+                    header_length = i + 4;
+                    header_received = 1;
+                    break;
+                }
+            }
+        }
+
+        if (header_received) {
+            fwrite(buffer + header_length, 1, bytes_received - header_length, fp);
+            header_length = 0;
+        }
+    }
+
+    fclose(fp);
+    closesocket(sock);
+    WSACleanup();
 }
 
 void extract_filenames(const char* html, FileInfo files[], int* file_count) {
@@ -119,27 +246,28 @@ void extract_filenames(const char* html, FileInfo files[], int* file_count) {
 void start_client() {
     struct mg_mgr mgr;
     struct mg_connection* c;
-    char ip[16];
-    int port;
 
     printf("Ingrese la dirección IP del servidor: ");
-    if (scanf("%15s", ip) != 1) {
+    if (scanf("%15s", ip_address) != 1) {
         fprintf(stderr, "Error al leer la dirección IP.\n");
-        return 1;
+        return;
     }
 
     printf("Ingrese el puerto del servidor: ");
     if (scanf("%d", &port) != 1) {
         fprintf(stderr, "Error al leer el puerto.\n");
-        return 1;
+        return;
     }
 
+    select_directory(download_directory);
+
     char server_url[32];
-    snprintf(server_url, sizeof(server_url), "http://%s:%d", ip, port);
+    snprintf(server_url, sizeof(server_url), "http://%s:%d", ip_address, port);
 
     mg_mgr_init(&mgr);
     system("cls");
-    printf("Cliente iniciado. Escuchando...\n");
+    printf("Cliente iniciado. Escuchando... (CTRL + SHIFT + ALT + W para minimizar)\n");
+
     printf("Listado de archivos:\n");
 
     while (1) {
@@ -157,4 +285,3 @@ void start_client() {
 
     mg_mgr_free(&mgr);
 }
-
--- src/client.h
+++ src/client.h
@@ -3,18 +3,19 @@
 #define CLIENT_H
 
 #include <stdint.h>
+#include "server.h"
 
 #define MAX_FILENAME_LEN 256
 #define MAX_FILES 100
 
 typedef struct {
-    char filename[MAX_FILENAME_LEN];
+    wchar_t filename[MAX_FILENAME_LEN];
     uint64_t timestamp;
 } FileInfo;
 
 void start_client();
-
 void extract_filenames(const char* html, FileInfo files[], int* file_count);
+void notificar(const char* message);
+void descargar_archivo(const char* filename);
 
-void notificar(const char* filename);
 #endif
